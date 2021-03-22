7f9a1d4f5fd9d48eb0b75a280353501b92492bc8
Guido Cella <guido@guidocella.xyz>
2021-03-22 12:06:57 +0100

    don't misdetect media with duration 0.* and 1.*

--- scripts/image-detection.lua
+++ scripts/image-detection.lua
@@ -1,7 +1,8 @@
 local was_image
 
 mp.register_event('file-loaded', function()
-    if (mp.get_property_number('duration') or 0) < 2 then
+    local duration = mp.get_property_number('duration')
+    if duration == nil or duration == 0 or duration == 1 then
         mp.command('show-text "[${playlist-pos-1}/${playlist-count}] ${filename} ${width}x${height} ${!gamma==0:☀}" 3000')
         -- Or set osd-msg1 to show text permanently.
 
