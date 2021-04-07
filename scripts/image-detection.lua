f6315f2e40270f8c4ea98fce8d1a0112fd7f9428
Guido Cella <guido@guidocella.xyz>
2021-04-07 14:34:46 +0200

    detect all 1-frame gifs
    Apparently many have 0.1 duration.

--- scripts/image-detection.lua
+++ scripts/image-detection.lua
@@ -2,7 +2,7 @@ local was_image
 
 mp.register_event('file-loaded', function()
     local duration = mp.get_property_number('duration')
-    if duration == nil or duration == 0 or duration == 1 then
+    if duration == nil or duration == 0 or duration == 0.1 or duration == 1 then
         mp.command('show-text "[${playlist-pos-1}/${playlist-count}] ${filename} ${width}x${height} ${!gamma==0:☀}" 3000')
         -- Or set osd-msg1 to show text permanently.
 
