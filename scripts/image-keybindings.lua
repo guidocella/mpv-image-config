26073d942d8479e63170c92a7750c0b0759c484d
Guido Cella <guido@guidocella.xyz>
2021-07-01 16:22:06 +0200

    fix rotation

--- scripts/image-keybindings.lua
+++ scripts/image-keybindings.lua
@@ -5,14 +5,7 @@ end)
 
 mp.register_script_message('pan-image', function (axis, amount)
     local dim = mp.get_property_native('osd-dimensions')
-    local width = dim.w - dim.ml - dim.mr
-    local height = dim.h - dim.mt - dim.mb
-    local dimension
-    if mp.get_property_number('video-rotate') % 180 == 0 then
-        dimension = axis == 'x' and width or height
-    else
-        dimension = axis == 'x' and height or width
-    end
+    local dimension = axis == 'x' and dim.w - dim.ml - dim.mr or dim.h - dim.mt - dim.mb
     local osd_dimension = axis == 'x' and dim.w or dim.h
     if dimension ~= osd_dimension then
         mp.commandv('add', 'video-align-' .. axis, amount * 2 * osd_dimension / (dimension - osd_dimension))
