b9694028ac54f7cbf4cabdb62b652652c2255f74
Guido Cella <guido@guidocella.xyz>
2021-07-21 16:45:24 +0200

    add align-to-cursor

--- scripts/image-keybindings.lua
+++ scripts/image-keybindings.lua
@@ -11,3 +11,21 @@ mp.register_script_message('pan-image', function (axis, amount)
         mp.commandv('add', 'video-align-' .. axis, amount * 2 * osd_dimension / (dimension - osd_dimension))
     end
 end)
+
+mp.add_key_binding(nil, 'align-to-cursor', function (table)
+    if table.event == 'up' then
+        mp.remove_key_binding('align-to-cursor-mouse-move')
+        return
+    end
+
+    local dims = mp.get_property_native('osd-dimensions')
+    mp.add_forced_key_binding('MOUSE_MOVE', 'align-to-cursor-mouse-move', function()
+        local mouse_pos = mp.get_property_native('mouse-pos')
+        if dims.w - dims.ml - dims.mr > dims.w  then
+            mp.set_property('video-align-x', (mouse_pos.x * 2 - dims.w) / dims.w)
+        end
+        if dims.h - dims.mt - dims.mb > dims.h  then
+            mp.set_property('video-align-y', (mouse_pos.y * 2 - dims.h) / dims.h)
+        end
+    end)
+end, { complex = true })
