83747ebda72d8d9aad819043a949d7d6e21210f5
Guido Cella <guido@guidocella.xyz>
2021-08-12 16:52:25 +0200

    reset align on rotations

--- scripts/detect-image.lua
+++ scripts/detect-image.lua
@@ -73,10 +73,10 @@ end)
 
 -- Align images bigger than the window to the top right corner.
 local dont_center
-mp.observe_property('dwidth', 'native', function (_, dwidth)
-    local dims = mp.get_property_native('osd-dimensions')
-    if not dwidth or not is_image() then return end
+mp.observe_property('video-out-params', 'native', function (_, params)
+    if not params or not is_image() then return end
 
+    local dims = mp.get_property_native('osd-dimensions')
     -- osd-width and osd-height, and even display-width and display-height, can be unavailable when the first image loads.
     if dims.w == 0 then
         dims.w = mp.get_property_native('display-width', 1920)
@@ -87,16 +87,15 @@ mp.observe_property('dwidth', 'native', function (_, dwidth)
         was_image = true
         if not options.first_unscaled then return end
 
-        local dheight = mp.get_property_native('dheight')
-        if not mp.get_property_bool('video-unscaled') and (dwidth > dims.w or dheight > dims.h) and mp.get_property_native('image-display-duration') == math.huge then
+        if not mp.get_property_bool('video-unscaled') and (params.dw > dims.w or params.dh > dims.h) and mp.get_property_native('image-display-duration') == math.huge then
             mp.set_property('video-unscaled', 'yes')
 
             -- dims = mp.get_property_native('osd-dimensions')
             -- this isn't recalculated immediately, so just align the image based on dwidth and dheight
-            if dwidth > dims.w then
+            if params.dw > dims.w then
                 mp.set_property('video-align-x', options.align_x)
             end
-            if dheight > dims.h then
+            if params.dh > dims.h then
                 mp.set_property('video-align-y', -1)
             end
             dont_center = true
