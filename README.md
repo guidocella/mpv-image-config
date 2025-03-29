8a7314eb4d5cab51bcfefe62aa21a743a798cf02
Guido Cella <guido@guidocella.xyz>
2025-03-29 18:47:47 +0100

    update for mpv 0.40

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ This is an example configuration for using mpv as an image viewer.
 
 ## Why?
 
-* mpv is feature rich and far more extensible than any image viewer. You can easily add original key bindings, set up conditional profiles and do pretty much anything with Lua scripts
-* Zoomed images look better than in image viewers (or at least the ones I previously used), and you can even improve image quality further with GLSL shaders like [FSR](https://gist.github.com/agyild/82219c545228d70c5604f865ce0b0ce5) and [Anime4k](https://github.com/bloc97/Anime4K)
+* mpv is feature rich and far more extensible than any image viewer. You can easily add key bindings, set up conditional profiles and do pretty much anything with scripts
+* Zoomed images look better than in image viewers with less advanced scaling
 * You can use quit-watch-later with directories of images
 * You can browse directories with both videos and images in one application
 * Background transparency
@@ -11,7 +11,9 @@ This is an example configuration for using mpv as an image viewer.
 
 ## Usage
 
-Copy what you want to use from `mpv.conf` and `input.conf` into your versions of these files, and copy or symlink into `~~/scripts` `scripts/align-images.lua` to automatically align the OSD to the top-right corner of images bigger than the OSD and to center images smaller than the OSD, and `scripts/image-bindings.lua` for the script messages used by `input.conf`.
+`positioning.lua` and `--video-recenter` have been upstreamed into mpv 0.40 (use https://github.com/guidocella/mpv-image-config/tree/931e71082f88ecb9afdd7728afb1ea9bc197c288 for mpv 0.39), so you just need to copy what you want to use from `mpv.conf` and `input.conf` into your versions of these files.
+
+`scripts/image-bindings.lua` is an optional script with extra script bindings I didn't consider worth upstreaming: a simple gesture, deletion of the current file and oneshot double page mode.
 
 ## How can I display thumbnails of images?
 
--- scripts/image-bindings.lua
+++ scripts/image-bindings.lua
@@ -13,18 +13,6 @@ mp.register_script_message('rm-file', function()
     mp.command('playlist-remove current')
 end)
 
-mp.register_script_message('pan-image', function (axis, amount)
-    local dims = mp.get_property_native('osd-dimensions')
-    local dimension = axis == 'x' and dims.w - dims.ml - dims.mr or dims.h - dims.mt - dims.mb
-    local osd_dimension = axis == 'x' and dims.w or dims.h
-    -- 1 video-align shifts the OSD by (dimension - osd_dimension) / 2 pixels
-    -- so the equation to find how much video-align to add to offset the OSD by osd_dimension is
-    -- x/1 = osd_dimension / ((dimension - osd_dimension) / 2)
-    if dimension > osd_dimension then
-        mp.commandv('add', 'video-align-' .. axis, amount * 2 * osd_dimension / (dimension - osd_dimension))
-    end
-end)
-
 local old_mouse_pos
 mp.add_key_binding(nil, 'gesture', function (table)
     if table.event == 'down' then
@@ -41,81 +29,6 @@ mp.add_key_binding(nil, 'gesture', function (table)
         mp.command(options[mouse_pos.x > old_mouse_pos.x and 'gesture_right' or 'gesture_left'])
     end
 end, { complex = true })
-
-mp.add_key_binding(nil, 'align-to-cursor', function (table)
-    if table.event == 'up' then
-        mp.remove_key_binding('align-to-cursor-mouse-move')
-        return
-    end
-
-    local dims = mp.get_property_native('osd-dimensions')
-    mp.add_forced_key_binding('MOUSE_MOVE', 'align-to-cursor-mouse-move', function()
-        local mouse_pos = mp.get_property_native('mouse-pos')
-        if dims.ml + dims.mr < 0 then
-            mp.set_property('video-align-x', (mouse_pos.x * 2 - dims.w) / dims.w)
-        end
-        if dims.mt + dims.mb < 0 then
-            mp.set_property('video-align-y', (mouse_pos.y * 2 - dims.h) / dims.h)
-        end
-    end)
-end, { complex = true })
-
-mp.add_key_binding(nil, 'drag-to-pan', function (table)
-    if table.event == 'up' then
-        mp.remove_key_binding('drag-to-pan-mouse-move')
-        return
-    end
-
-    local dims = mp.get_property_native('osd-dimensions')
-    local old_mouse_pos = mp.get_property_native('mouse-pos')
-    local old_video_align_x = mp.get_property_native('video-align-x')
-    local old_video_align_y = mp.get_property_native('video-align-y')
-    mp.add_forced_key_binding('MOUSE_MOVE', 'drag-to-pan-mouse-move', function()
-        local mouse_pos = mp.get_property_native('mouse-pos')
-        -- 1 video-align shifts the OSD by (dimension - osd_dimension) / 2 pixels
-        -- so the equation to find how much video-align to add to offset the OSD by the difference in mouse position is
-        -- x/1 = (mouse_pos - old_mouse_pos) / ((dimension - osd_dimension) / 2)
-        if dims.ml + dims.mr < 0 then
-            mp.set_property(
-                'video-align-x',
-                math.min(1, math.max(old_video_align_x + 2 * (mouse_pos.x - old_mouse_pos.x) / (dims.ml + dims.mr), -1))
-            )
-        end
-        if dims.mt + dims.mb < 0 then
-            mp.set_property(
-                'video-align-y',
-                math.min(1, math.max(old_video_align_y + 2 * (mouse_pos.y - old_mouse_pos.y) / (dims.mt + dims.mb), -1))
-            )
-        end
-    end)
-end, { complex = true })
-
-mp.register_script_message('cursor-centric-zoom', function (amount)
-    local mouse_pos = mp.get_property_native('mouse-pos')
-    local dims = mp.get_property_native('osd-dimensions')
-    local width = (dims.w - dims.ml - dims.mr) * 2^amount
-    local height = (dims.h - dims.mt - dims.mb) * 2^amount
-    local command = 'no-osd add video-zoom ' .. amount .. ';'
-
-    if width > dims.w then
-        local old_cursor_ml = dims.ml - mouse_pos.x
-        local cursor_ml = old_cursor_ml * 2^amount
-        local ml = cursor_ml + mouse_pos.x
-        -- from video/out/aspect.c:src_dst_split_scaling() we know that:
-        -- ml = (osd-width - width) * (video-align-x + 1) / 2
-        -- so video-align-x is:
-        local video_align_x = 2 * ml / (dims.w - width) - 1
-        command = command .. 'no-osd set video-align-x ' .. math.min(1, math.max(video_align_x, -1)) .. ';'
-    end
-
-    if height > dims.h then
-        local mt = (dims.mt - mouse_pos.y) * 2^amount + mouse_pos.y
-        local video_align_y = 2 * mt / (dims.h - height) - 1
-        command = command .. 'no-osd set video-align-y ' .. math.min(1, math.max(video_align_y, -1))
-    end
-
-    mp.command(command)
-end)
 
 local is_intial_callback
 local function undo_lavfi_complex()
